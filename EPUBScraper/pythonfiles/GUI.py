52f66365fcf7c62bab847641ffd40cf09ee0c5e9
AlvinValdez <32502043+AlvinValdez@users.noreply.github.com>
2021-10-15 16:31:48 -0400

    Added multithreaded functionality

--- EPUBScraper/pythonfiles/GUI.py
+++ EPUBScraper/pythonfiles/GUI.py
@@ -1,25 +1,42 @@
 import sys
 from wuxia import scrape
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QComboBox, QPushButton,QWidget, QLineEdit
+from PyQt5.QtCore import QRunnable, Qt, QThreadPool
+
+class Runnable(QRunnable):
+
+    def __init__(self, fchap, lchap, site):
+
+        super().__init__()
+
+        self.fchap = fchap
+        self.lchap = lchap
+        self.site = site
+
+
+    def run(self):
+
+        scrape(int(self.fchap),int(self.lchap),self.site)
+
 
 
 class Gui(QMainWindow):
 
     def __init__(self):
-        super().__init__()
+        super(Gui,self).__init__()
 
         fchaplabel= QLabel(self)
         lchaplabel = QLabel(self)
-        fchap = QLineEdit(self)
-        lchap = QLineEdit(self)
-        site = QLineEdit(self)
+        self.fchap = QLineEdit(self)
+        self.lchap = QLineEdit(self)
+        self.site = QLineEdit(self)
 
-        fchap.move(25,100)
+        self.fchap.move(25,100)
         fchaplabel.setText("First Chapter")
         fchaplabel.move(45,75)
 
 
-        lchap.move(200,100)
+        self.lchap.move(200,100)
         lchaplabel.setText("Last Chapter")
         lchaplabel.move(220,75)
 
@@ -27,10 +44,10 @@ class Gui(QMainWindow):
         button.setToolTip('Click here when ready to scrape!')
         button.move(110, 150)
 
-        button.clicked.connect(lambda: self.on_click(fchap.text(),lchap.text(),site.text()))
+        button.clicked.connect(self.runTasks)
 
-        site.resize(315,25)
-        site.move(10, 25)
+        self.site.resize(315,25)
+        self.site.move(10, 25)
 
 
 
@@ -42,14 +59,11 @@ class Gui(QMainWindow):
         self.setWindowTitle("QLineEdit Example")
         self.show()
 
+    def runTasks(self):
+        pool = QThreadPool.globalInstance()
+        runnable = Runnable(self.fchap.text(),self.lchap.text(),self.site.text())
+        pool.start(runnable)
 
 
-    def on_click(self,fchap,lchap,novel):
-            scrape(int(fchap),int(lchap),novel)
 
 
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
