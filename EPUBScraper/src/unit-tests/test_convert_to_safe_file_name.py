6d43eb58900d063622dbe5f28736527e830bbee2
AlvinValdez <32502043+AlvinValdez@users.noreply.github.com>
2021-11-11 23:03:41 -0500

    Update test_convert_to_safe_file_name.py

--- EPUBScraper/src/unit-tests/test_convert_to_safe_file_name.py
+++ EPUBScraper/src/unit-tests/test_convert_to_safe_file_name.py
@@ -1,6 +1,7 @@
 import sys
-from convert_to_file_name.convert_to_safe_file_name import convert_to_file_name
+import convert_to_file_name.convert_to_safe_file_name as convert
+
 
 def test_convert_to_file_name():
-    assert convert_to_file_name('A Record of a Mortal’s Journey to Immortality') == 'A-Record-of-a-Mortal’s-Journey-to-Immortality'
-    assert convert_to_file_name('Rebirth of a Fashionista: This Life Is Soo Last Season') == 'Rebirth-of-a-Fashionista-This-Life-Is-Soo-Last-Season'
+    assert convert.convert_to_file_name('A Record of a Mortal’s Journey to Immortality') == 'A-Record-of-a-Mortal’s-Journey-to-Immortality'
+    assert convert.convert_to_file_name('Rebirth of a Fashionista: This Life Is Soo Last Season') == 'Rebirth-of-a-Fashionista-This-Life-Is-Soo-Last-Season'
